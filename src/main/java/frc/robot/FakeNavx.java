e8a8144a8461da8510c1ba2cb63a3200290adda0
zzberries <24paliti@nsboroschools.net>
2021-03-23 22:21:06 -0400

    implements angular velocity update logic

--- src/main/java/frc/robot/FakeNavx.java
+++ src/main/java/frc/robot/FakeNavx.java
@@ -10,11 +10,22 @@ import edu.wpi.first.wpilibj.SPI.Port;
 public class FakeNavx {
     Port m_port;
     double m_yaw;
+    public static final double MAX_DEGREES_PER_SECOND = 180;
+
     public FakeNavx(Port NAVX_PORT) {
         m_port = NAVX_PORT;
     }
+
     public double getYaw() {
         return m_yaw;
     }
-    
+
+    // time is in seconds for update function
+    public void update(double time, double motorSpeed) {
+        double velocity = motorSpeed * MAX_DEGREES_PER_SECOND;
+        if (motorSpeed >= -0.3 && motorSpeed <= 0.3 ) {
+            velocity = 0;
+        }
+        m_yaw = velocity * time + m_yaw;
+    }
 }
