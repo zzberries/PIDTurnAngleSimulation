bc71cb5777f86de26be908c9fb39be047a8cedb3
Thor Smith <thorsmith777@gmail.com>
2021-03-24 20:34:35 -0400

    maps -180 to 180 for navx yaw value
    This change maps -180 to 180 for navx yaw value.
    It also increases the max velocity (degrees per second) that
    the motors can cause. Friendly comments are also added.

--- src/main/java/frc/robot/FakeNavx.java
+++ src/main/java/frc/robot/FakeNavx.java
@@ -10,7 +10,7 @@ import edu.wpi.first.wpilibj.SPI.Port;
 public class FakeNavx {
     Port m_port;
     double m_yaw;
-    public static final double MAX_DEGREES_PER_SECOND = 180;
+    public static final double MAX_DEGREES_PER_SECOND = 360;
 
     public FakeNavx(Port NAVX_PORT) {
         m_port = NAVX_PORT;
@@ -22,10 +22,26 @@ public class FakeNavx {
 
     // time is in seconds for update function
     public void update(double time, double motorSpeed) {
+        // calculate the angular velocity from the motor speed
         double velocity = motorSpeed * MAX_DEGREES_PER_SECOND;
+        // set up a "dead band" from -0.3 to 0.3 where the
+        // motors are unable to turn the robot because they
+        // cannot overcome friction.
         if (motorSpeed >= -0.3 && motorSpeed <= 0.3 ) {
             velocity = 0;
         }
+        // calculate new angle based on angular velocity
         m_yaw = velocity * time + m_yaw;
+
+        // wrap the angle around if it exceeds 180 degrees or
+        // goes below -180
+        if (m_yaw > 180) {
+            double extra = m_yaw % 180;
+            m_yaw = -180 + extra;
+        }
+        if (m_yaw < -180) {
+            double extra = m_yaw % 180;
+            m_yaw = 180 + extra;
+        }
     }
 }
